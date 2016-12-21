fcb6eb38bdc57f0e088e68a5036a8434cb6ec84e
Daniel Høyer Iversen <mail@dahoiv.net>
2016-12-21 15:04:54 +0100

    Update __init__.py

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -175,8 +175,13 @@ class device:
     aes = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
     payload = aes.decrypt(bytes(enc_payload))
     if payload:
+      key = payload[0x04:0x14]
+      try:
+        aes = AES.new(bytes(key), AES.MODE_CBC, bytes(self.iv))
+      except ValueError:
+        return False
       self.id = payload[0x00:0x04]
-      self.key = payload[0x04:0x14]
+      self.key = key
       return True
     else:
       return False
