256468fa81a2ec78ade6ca831aaf16bb961d0782
Daniel Høyer Iversen <mail@dahoiv.net>
2016-12-25 23:06:34 +0100

    Update __init__.py

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -46,18 +46,21 @@ def gendevice(devtype, host, mac):
     return rm(host=host, mac=mac)
   elif devtype == 0x2714: # A1
     return a1(host=host, mac=mac)
+  elif devtype == 0x4EB5: # MP1
+    return mp1(host=host, mac=mac)
   else:
     return device(host=host, mac=mac)
 
-def discover(timeout=None):
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.connect(('8.8.8.8', 53))  # connecting to a UDP address doesn't send packets
-  local_ip_address = s.getsockname()[0]
+def discover(timeout=None, local_ip_address=None):
+  if local_ip_address is None:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect(('8.8.8.8', 53))  # connecting to a UDP address doesn't send packets
+      local_ip_address = s.getsockname()[0]
   address = local_ip_address.split('.')
   cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   cs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   cs.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-  cs.bind(('',0))
+  cs.bind((local_ip_address,0))
   port = cs.getsockname()[1]
   starttime = time.time()
 
@@ -125,6 +128,7 @@ def discover(timeout=None):
       devices.append(dev)
     return devices
 
+
 class device:
   def __init__(self, host, mac, timeout=10):
     self.host = host
@@ -174,18 +178,9 @@ class device:
 
     aes = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
     payload = aes.decrypt(bytes(enc_payload))
-    if not payload:
-      return False
-
-    key = payload[0x04:0x14]
-    try:
-      aes = AES.new(bytes(key), AES.MODE_CBC, bytes(self.iv))
-    except ValueError:
-      return False
 
     self.id = payload[0x00:0x04]
-    self.key = key
-    return True
+    self.key = payload[0x04:0x14]
 
   def get_type(self):
     return self.type
@@ -222,7 +217,6 @@ class device:
       checksum += payload[i]
       checksum = checksum & 0xffff
 
-    print("key, iv:  ", self.key, self.iv)
     aes = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
     payload = aes.encrypt(bytes(payload))
 
@@ -252,6 +246,65 @@ class device:
             pass
           raise
     return bytearray(response[0])
+
+
+class mp1(device):
+  def __init__ (self, host, mac):
+    device.__init__(self, host, mac)
+    self.type = "MP1"
+
+  def set_power_mask(self, sid_mask, state):
+    """Sets the power state of the smart power strip."""
+
+    packet = bytearray(16)
+    packet[0x00] = 0x0d
+    packet[0x02] = 0xa5
+    packet[0x03] = 0xa5
+    packet[0x04] = 0x5a
+    packet[0x05] = 0x5a
+    packet[0x06] = 0xb2 + ((sid_mask<<1) if state else sid_mask)
+    packet[0x07] = 0xc0
+    packet[0x08] = 0x02
+    packet[0x0a] = 0x03
+    packet[0x0d] = sid_mask
+    packet[0x0e] = sid_mask if state else 0
+
+    response = self.send_packet(0x6a, packet)
+
+    err = response[0x22] | (response[0x23] << 8)
+
+  def set_power(self, sid, state):
+    """Sets the power state of the smart power strip."""
+    sid_mask = 0x01 << (sid - 1)
+    return self.set_power_mask(sid_mask, state)
+
+  def check_power(self):
+    """Returns the power state of the smart power strip."""
+    packet = bytearray(16)
+    packet[0x00] = 0x0a
+    packet[0x02] = 0xa5
+    packet[0x03] = 0xa5
+    packet[0x04] = 0x5a
+    packet[0x05] = 0x5a
+    packet[0x06] = 0xae
+    packet[0x07] = 0xc0
+    packet[0x08] = 0x01
+
+    response = self.send_packet(0x6a, packet)
+    err = response[0x22] | (response[0x23] << 8)
+    if err == 0:
+      aes = AES.new(bytes(self.key), AES.MODE_CBC, bytes(self.iv))
+      payload = aes.decrypt(bytes(response[0x38:]))
+      if type(payload[0x4]) == int:
+        state = payload[0x0e]
+      else:
+        state = ord(payload[0x0e])
+      data = {}
+      data['s1'] = bool(state & 0x01)
+      data['s2'] = bool(state & 0x02)
+      data['s3'] = bool(state & 0x04)
+      data['s4'] = bool(state & 0x08)
+      return data
 
 
 class sp1(device):
